a8aed4131eabb9afb3ce6c8fcf266c0fbb4abe47
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-09-12 18:35:39 -0400

    update getInterview funcation

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,14 +7,23 @@ export function getAppointmentsForDay(state, day) {
   
   return filteredDays;
 }
+export function getInterviewersForDay(state, day) {
+  const filteredInterviewer = state.days.find(({name})=> name === day);
+  if (state.days.length === 0 || !filteredInterviewer) {
+    return [];
+  }
+  const filteredInterviewers = filteredInterviewer.interviewers.map((appointmentId) => state.interviewers[appointmentId])
+  
+  return filteredInterviewers;
+}
 
 export function getInterview(state, interview) {
   if(!interview) return null;
 
-  const interviewerArray = Object.values(state.interviewers);
+  // const interviewerArray = Object.values(state.interviewers);
   const student = interview.student;
-  const interviewer = interviewerArray.find(({id}) => id === interview.interviewer);
+  // const interviewer = interviewerArray.find(({id}) => id === interview.interviewer);
 
-  return {student, interviewer}
+  return {student, interviewer: state.interviewers[interview.interviewer]}
 
 }
